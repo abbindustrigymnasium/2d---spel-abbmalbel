2ed1459ae9cb31ba4ca7378288ad2d5d9eaba1f1
abbmalbel <69853581+abbmalbel@users.noreply.github.com>
2021-05-09 11:17:42 +0200

    Animationer
    fixade animationer för slide, jump, och  run

--- Spel projekt/Assets/Scripts/PlayerMovement.cs
+++ Spel projekt/Assets/Scripts/PlayerMovement.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour {
 
 	public CharacterController2D controller;
+	public Animator animator;
 
 	public float runSpeed = 40f;
 
@@ -14,28 +14,42 @@ public class PlayerMovement : MonoBehaviour
 	bool crouch = false;
 
 	// Update is called once per frame
-	void Update()
-	{
+	void Update () {
 
 		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
 		if (Input.GetButtonDown("Jump"))
 		{
 			jump = true;
+			animator.SetBool("IsJumping", true);
+		
 		}
 
 		if (Input.GetButtonDown("Crouch"))
 		{
 			crouch = true;
-		}
-		else if (Input.GetButtonUp("Crouch"))
+			
+
+		} else if (Input.GetButtonUp("Crouch"))
 		{
 			crouch = false;
 		}
 
 	}
 
-	void FixedUpdate()
+	public void OnLanding ()
+	{
+		animator.SetBool("IsJumping", false);
+	}
+
+	public void OnCrouching (bool isCrouching)
+	{
+		animator.SetBool("IsCrouching", isCrouching);
+	}
+
+	void FixedUpdate ()
 	{
 		// Move our character
 		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
